33ba4f79c1f48dcb5461ad0fea5e3393c7b83f71
tishay <tmobuse@gmail.com>
2024-04-16 13:04:32 -0500

    new commit

--- area_of_a_circle.py
+++ area_of_a_circle.py
@@ -3,10 +3,12 @@ def compute_area_of_circle(radius):
 	area = pi * radius * radius
 	return area
 
-radius1 = 30
+radius1 = 40
 area1 = compute_area_of_circle(radius1)
 print(f"The area of circle with radius {radius1} is: {area1}")
 
-radius2 = 40
+radius2 = 50
 area2 = compute_area_of_circle(radius2)
 print(f"The area of circle with radius {radius2} is: {area2}")
+
+#new commit
